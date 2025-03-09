fc6d836b97412373c2a35a1457c71e66867b8feb
Amit kumar <140205046+amitkumar822@users.noreply.github.com>
2025-03-09 09:50:13 +0530

    feat: update CORS configuration to use environment variable for origin

--- backend/app.js
+++ backend/app.js
@@ -6,19 +6,19 @@ import errorHandler from "./src/middlewares/errorHandler.js";
 
 const app = express();
 
-app.use(
-  cors({
-    origin:"*",
-    credentials: true,
-  })
-);
-
 // app.use(
 //   cors({
-//     origin: process.env.CORS_ORIGIN,
+//     origin:"*",
 //     credentials: true,
 //   })
 // );
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
